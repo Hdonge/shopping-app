6663fdd416b7b6509e546bef68b52db3c3df6e6c
Hdonge <harshal.donge@gmail.com>
2018-08-12 11:13:52 +0530

    Refactored components eventemitter code to recipe service to improve cross component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model'
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Test Recipe', 'This is simply test', 'http://www.maoritelevision.com/sites/default/files/styles/news_article_full/public/Kai%20Moana%20Paella.jpg'),
         new Recipe('Paella De Pollo', 'This is simply test', 'http://www.maoritelevision.com/sites/default/files/styles/news_article_full/public/Palle%20de%20pollo.png')

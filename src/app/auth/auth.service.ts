967c5f1f550dc75e3d3b28f09dc256a0347b1b1a
Hdonge <harshal.donge@gmail.com>
2018-08-26 22:14:29 +0530

    Signing users up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,10 @@
-export class AuthService {
-    signupUser(email: string, password: string) { 
+import * as firebase from "firebase";
 
+export class AuthService {
+    signupUser(email: string, password: string) {
+        console.log("Hello");
+        firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password).catch(error => {
+            console.log(error);
+        });
     }
 }

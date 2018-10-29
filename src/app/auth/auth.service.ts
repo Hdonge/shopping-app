dadc9a74d2a00527f9fe89baf351bf39406a05b9
Hdonge <harshal.donge@gmail.com>
2018-10-29 17:41:04 +0530

    Temp auth  commit - need to look further

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+    token: string;
     signupUser(email: string, password: string) {
         firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password).catch(error => {
             console.log(error);
@@ -10,9 +11,14 @@ export class AuthService {
     signinUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(response => {
-                console.log(response);
+                /* firebase.auth().currentUser.getIdToken()
+                .then() */
             }).catch(error => {
                 console.log(error);
             });
     }
+
+    /* getToken() {
+        return firebase.auth().currentUser.getIdToken()
+    } */
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,16 +3,23 @@ import { Http, Response } from '@angular/http';
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) { }
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
 
     storeRecipes() {
         return this.http.put('https://ng-recipe-book-6ce44.firebaseio.com/recipes.json', this.recipeService.getRecipes());
     }
 
     getRecipes() {
+        /* let tk = '';
+        this.authService.getToken()
+            .then((token: string) => {
+                tk = token;
+            }); */
+
         this.http.get('https://ng-recipe-book-6ce44.firebaseio.com/recipes.json').subscribe((response: Response) => {
             const recipes: Recipe[] = response.json();
             this.recipeService.setRecipes(recipes);
